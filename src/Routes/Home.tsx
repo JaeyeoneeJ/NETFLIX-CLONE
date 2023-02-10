d140150d85382c3369b3ba0ffca8be2f744c6c9d
jaeyeoneej <5ikve@naver.com>
2023-02-10 22:37:52 +0900

    fixed: #1 Home 일부 CSS 변경 23-02-10

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,5 @@
 function Home() {
-  return <div style={{ backgroundColor: "black", height: "200vh" }}></div>;
+  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
 }
 
 export default Home;
