6776baea50283ddac6d598c69d5d2a3da08afe80
jaeyeoneej <5ikve@naver.com>
2023-02-11 12:48:49 +0900

    add: #2 Header 내 Input 컴포넌트 및 animate 생성 23-02-11
    - 돋보기 모양 svg를 토글 시 Input의 x 길이를 조정하게 함
    - 돋보기와 Input 모두 animate type이 spring이어서 통일성 등을 위해 type을 linear로 변경

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { motion, Variants } from "framer-motion";
 import { Link, useMatch } from "react-router-dom";
+import { useState } from "react";
 
 const Nav = styled.nav`
   display: flex;
@@ -52,6 +53,9 @@ const Item = styled.li`
 
 const Search = styled.span`
   color: white;
+  align-items: center;
+  display: flex;
+  position: relative;
   svg {
     height: 25px;
   }
@@ -69,6 +73,12 @@ const Circle = styled(motion.span)`
   margin: 0 auto;
 `;
 
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  left: -150px;
+`;
+
 const logoVariants: Variants = {
   normal: {
     fillOpacity: 1,
@@ -82,9 +92,11 @@ const logoVariants: Variants = {
 };
 
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("/tv");
-  console.log(homeMatch, tvMatch);
+
+  const toggleSearch = () => setSearchOpen((prev) => !prev);
   return (
     <Nav>
       <Col>
@@ -119,7 +131,10 @@ function Header() {
       </Col>
       <Col>
         <Search>
-          <svg
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -180 : 0 }}
+            transition={{ type: "linear" }}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -129,7 +144,12 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+            animate={{ scaleX: searchOpen ? 1 : 0 }}
+            transition={{ type: "linear" }}
+            placeholder="Search for movie or tv show..."
+          />
         </Search>
       </Col>
     </Nav>
