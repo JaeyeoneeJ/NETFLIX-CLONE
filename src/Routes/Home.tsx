81b27a48d7e9baf8a06217392160a6f8e22b383b
jaeyeoneej <5ikve@naver.com>
2023-02-13 14:00:22 +0900

    add: #3 메인 Banner 생성 23-02-13
    - Image url을 쉽게 가져오기 위해 utils.ts를 만들어 가져옴.
    - utils.ts의 format은 있을 경우 그대로, 없을 경우 original(원본 사이즈)를 불러오도록 함
    - Banner 컴포넌트에 Img url을 prop으로 넘기고, data fetching이 되지 않았을 경우, 빈 string ""을 불러오게 함
    - background-image로 linear gradient 배경과 img url을 줌

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
 
 const Wrapper = styled.div`
   background: black;
@@ -13,13 +14,47 @@ const Loader = styled.div`
   align-items: center;
 `;
 
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+const Overview = styled.p`
+  font-size: 36px;
+  width: 50%;
+`;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
   console.log(data, isLoading);
-  return <Wrapper>{isLoading ? <Loader>Loading...</Loader> : null}</Wrapper>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
