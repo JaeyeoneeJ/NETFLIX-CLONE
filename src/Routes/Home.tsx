35296945fabf2c89a519701b24bb6159c1748548
jaeyeoneej <5ikve@naver.com>
2023-02-13 15:37:53 +0900

    fixed: #0 Slider animate 수정 23-02-13
    - Slider 테스트 중 Banner를 두번 이상 빠르게 클릭했을 때 간격이 벌어지는 현상 발생
    - AnimatePresense에 onExitComplete prop은 exit 되었을 때 해당 함수를 실행시킴
    - toggleLeaving 함수를 만들어 exit 되었을 때 leaving 상태를 바꾸게 하고,
    - leaving이 true일 때 Slider가 animate하지 않게 함

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -38,6 +38,7 @@ const Overview = styled.p`
 
 const Slider = styled.div`
   position: relative;
+  top: -100px;
 `;
 
 const Row = styled(motion.div)`
@@ -73,7 +74,13 @@ function Home() {
     getMovies
   );
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (leaving) return;
+    toggleLeaving();
+    setIndex((prev) => prev + 1);
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
@@ -88,7 +95,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
