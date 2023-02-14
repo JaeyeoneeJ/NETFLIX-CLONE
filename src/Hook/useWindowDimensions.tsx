912e6df27c0d5ac2c6d6aadf8f0a911e8e82e874
jaeyeoneej <5ikve@naver.com>
2023-02-14 13:30:31 +0900

    feat: #3 반응형 브라우저 width 값 추적을 위한 hook 생성 23-02-14

---
+++ src/Hook/useWindowDimensions.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width } = window;
+  return width;
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState<number>(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+export default useWindowDimensions;
